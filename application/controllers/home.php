f6340e7b9b05c2c2ae9291d3cbe9e281b06482e5
yinyiibin <yinyibin@woshop.cn>
2017-06-13 17:26:46 +0800

    asda

---
+++ application/controllers/home.php
@@ -0,0 +1,28 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * 后台管理首页
+ * add by yyb5683@gmail.com
+ * 2017年6月12日16:31:04
+ */
+class Home extends MY_Controller{
+	/**
+	 * 初始化
+	 * add by yyb5683@gmail.com
+	 * 2017年6月12日16:32:00
+	 */
+	public function __construct(){
+		parent::__construct();
+	}
+	
+	/**
+	 * 管理首页面
+	 * add by  yyb5683@gmail.com
+	 * 2017年6月12日16:32:21
+	 */
+	public function index(){
+		echo '1';die;
+		$this->load->view("main/index",$this->template);
+	}
+	
+} 
