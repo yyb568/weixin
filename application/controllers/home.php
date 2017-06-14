05f64f393dba28005e3a927bd5b46afbd93a7788
yinyiibin <yinyibin@woshop.cn>
2017-06-14 09:15:30 +0800

    添加验证

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -5,6 +5,7 @@
  * add by yyb5683@gmail.com
  * 2017年6月12日16:31:04
  */
+define('TOKEN','yinyibin');
 class Home extends MY_Controller{
 	/**
 	 * 初始化
@@ -21,7 +22,45 @@ class Home extends MY_Controller{
 	 * 2017年6月12日16:32:21
 	 */
 	public function index(){
-		$this->load->view("main/index",$this->template);
+		$this->valid();
+// 		$this->load->view("main/index",$this->template);
 	}
 	
+	/**
+	 * 首次接入验证
+	 * add by  yyb5683@gmail.com
+	 * 2017年6月14日09:13:51
+	 */
+	public function valid(){
+		$echoStr = $_GET["echostr"];
+	
+		//valid signature , option
+		if($this->checkSignature()){
+			echo $echoStr;
+			exit;
+		}
+	}
+	
+	/**
+	 * 首次接入验证
+	 * add by  yyb5683@gmail.com
+	 * 2017年6月14日09:13:51
+	 */
+	private function checkSignature(){
+		$signature = $_GET["signature"];
+		$timestamp = $_GET["timestamp"];
+		$nonce = $_GET["nonce"];
+	
+		$token = TOKEN;
+		$tmpArr = array($token, $timestamp, $nonce);
+		sort($tmpArr);
+		$tmpStr = implode( $tmpArr );
+		$tmpStr = sha1( $tmpStr );
+	
+		if( $tmpStr == $signature ){
+			return true;
+		}else{
+			return false;
+		}
+	}
 } 
