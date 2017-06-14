03b4dd3e4072fede1bdf94a81c5774d77999897c
yinyiibin <yinyibin@woshop.cn>
2017-06-14 08:40:32 +0800

    诉讼费

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,92 +1,27 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * 后台管理首页
+ * add by yyb5683@gmail.com
+ * 2017年6月12日16:31:04
+ */
+class Home extends MY_Controller{
+	/**
+	 * 初始化
+	 * add by yyb5683@gmail.com
+	 * 2017年6月12日16:32:00
+	 */
+	public function __construct(){
+		parent::__construct();
+	}
 	
-traceHttp();
-
-define("TOKEN", "yinyibin");
-$wechatObj = new wechatCallbackapiTest();
-if (isset($_GET['echostr'])) {
-    $wechatObj->valid();
-}else{
-    $wechatObj->responseMsg();
-}
-
-class wechatCallbackapiTest extends MY_Controller
-{
-    public function valid()
-    {
-        $echoStr = $_GET["echostr"];
-        if($this->checkSignature()){
-            echo $echoStr;
-            exit;
-        }
-    }
-
-    private function checkSignature()
-    {
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];
-
-        $token = TOKEN;
-        $tmpArr = array($token, $timestamp, $nonce);
-        sort($tmpArr);
-        $tmpStr = implode( $tmpArr );
-        $tmpStr = sha1( $tmpStr );
-
-        if( $tmpStr == $signature ){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    public function responseMsg()
-    {
-        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-
-        if (!empty($postStr)){
-            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-            $fromUsername = $postObj->FromUserName;
-            $toUsername = $postObj->ToUserName;
-            $keyword = trim($postObj->Content);
-            $time = time();
-            $textTpl = "<xml>
-                        <ToUserName><![CDATA[%s]]></ToUserName>
-                        <FromUserName><![CDATA[%s]]></FromUserName>
-                        <CreateTime>%s</CreateTime>
-                        <MsgType><![CDATA[%s]]></MsgType>
-                        <Content><![CDATA[%s]]></Content>
-                        <FuncFlag>0</FuncFlag>
-                        </xml>";
-            if($keyword == "?" || $keyword == "？")
-            {
-                $msgType = "text";
-                $contentStr = date("Y-m-d H:i:s",time());
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                echo $resultStr;
-            }
-        }else{
-            echo "";
-            exit;
-        }
-    }
-}
-
-function traceHttp()
-{
-    logger("\n\nREMOTE_ADDR:".$_SERVER["REMOTE_ADDR"].(strstr($_SERVER["REMOTE_ADDR"],'101.226')? " FROM WeiXin": "Unknown IP"));
-    logger("QUERY_STRING:".$_SERVER["QUERY_STRING"]);
-}
-function logger($log_content)
-{
-    if(isset($_SERVER['HTTP_APPNAME'])){   //SAE
-        sae_set_display_errors(false);
-        sae_debug($log_content);
-        sae_set_display_errors(true);
-    }else{ //LOCAL
-        $max_size = 500000;
-        $log_filename = "log.xml";
-        if(file_exists($log_filename) and (abs(filesize($log_filename)) > $max_size)){unlink($log_filename);}
-        file_put_contents($log_filename, date('Y-m-d H:i:s').$log_content."\r\n", FILE_APPEND);
-    }
-}
+	/**
+	 * 管理首页面
+	 * add by  yyb5683@gmail.com
+	 * 2017年6月12日16:32:21
+	 */
+	public function index(){
+		$this->load->view("main/index",$this->template);
+	}
+	
+} 
